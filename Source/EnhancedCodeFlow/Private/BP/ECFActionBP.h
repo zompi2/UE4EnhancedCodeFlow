b859cac934309fd8b4333d84a09d7829bdb9c4bb
Damian Nowakowski <damnowgry@gmail.com>
2022-07-19 20:35:36 +0200

    changelog and comments

--- Source/EnhancedCodeFlow/Private/BP/ECFActionBP.h
+++ Source/EnhancedCodeFlow/Private/BP/ECFActionBP.h
@@ -14,15 +14,31 @@ class ENHANCEDCODEFLOW_API UECFActionBP : public UBlueprintAsyncActionBase
 
 public:
 
+	/**
+	 * Initializes the Async Action with project WorldContext Object and Settings.
+	 * Set the action to start paused, as unreal will run it in next tick.
+	 */
 	virtual void Init(UObject* WorldContextObject, FECFActionSettings& Settings);
+
+	/**
+	 * Getting world from World Context Object.
+	 */
 	class UWorld* GetWorld() const override;
+
+	/**
+	 * Activate the action by resuming it (we always start it as paused).
+	 */
 	void Activate() override;
 
 protected:
 
+	// The World Context Object that started this action.
 	UPROPERTY(Transient)
 	class UObject* Proxy_WorldContextObject;
 
+	// Handle of the Action to control.
 	FECFHandle Proxy_Handle;
+
+	// If the action was supposed to be paused - to not resume it on Activate!
 	bool Proxy_IsPausedAtStart;
 };
