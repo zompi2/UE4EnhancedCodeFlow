dfd807f9307fec6d470ae2de1fed7f227b04b607
Damian Nowakowski <damnowgry@gmail.com>
2024-02-04 12:36:43 +0100

    run async then BP version

---
+++ Source/EnhancedCodeFlow/Private/BP/Actions/ECFRunAsyncThenBP.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2024 Damian Nowakowski. All rights reserved.
+
+#pragma once
+
+#include "../ECFActionBP.h"
+#include "ECFRunAsyncThenBP.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnECFRunAsyncThenBPEvent, bool, bTimedOut, bool, bStopped);
+
+UCLASS()
+class ENHANCEDCODEFLOW_API UECFRunAsyncThenBP : public UECFActionBP
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(BlueprintAssignable)
+	FOnECFRunAsyncThenBPEvent AsyncTask;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnECFRunAsyncThenBPEvent OnExecute;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", WorldContext = "WorldContextObject", AdvancedDisplay = "Settings", ToolTip = "Runs the given task function on a separate thread and calls the callback function when this task ends.\nSet TimeOut greater than 0 to stop action after this time.", DisplayName = "ECF - Run Async Then"), Category = "ECF")
+	static UECFRunAsyncThenBP* ECFRunAsyncThen(const UObject* WorldContextObject, float InTimeOut, FECFActionSettings Settings, FECFHandleBP& Handle);
+};
